34f0bec60f3af70ad5de24c8b6af1519ae5f7e7a
Sara Lonngren <saralonngren@gmail.com>
2020-01-15 22:06:03 -0300

    Created api call and improved ap.js

--- web/src/App.js
+++ web/src/App.js
@@ -1,90 +1,109 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api'
+
 import './global.css'
 import './App.css'
 import './Sidebar.css'
 import './Main.css'
 
-
 function App() {
+  const [devs, setDevs] = useState([])
+
+  const [latitude, setLatitude] = useState('')
+  const [longitude, setLongitude] = useState('')
+  const [github_username, setGithubUsername] = useState('')
+  const [techs, setTechs] = useState('')
+
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords
+
+        setLatitude(latitude)
+        setLongitude(longitude)
+      },
+      (err) => {
+        console.log(err)
+      },
+      {
+        timeout: 30000,
+      }
+    )
+  }, [])
+
+  useEffect(() => {
+    async function loadDev() {
+      const response = await api.get('/devs')
+
+      setDevs(response.data)
+    }
+
+    loadDev()
+  }, [])
+
+  async function handleAddDev(e){
+    e.preventDefault()
+
+    const response = await api.post('/devs', {
+      github_username,
+      techs,
+      latitude,
+      longitude,
+    })
+
+    setGithubUsername('')
+    setTechs('')
+
+    setDevs([...devs, response.data])
+  }
+
   return(
     <div id="app">
     <aside>
       <strong>Cadastrar</strong>
-      <form>
-        <div class="input-block">
+      <form onSubmit={handleAddDev}>
+        <div className="input-block">
           <label htmlFor="github_username">Usuário do Github</label>
-          <input name="github_username" id="github_username" required/>
+          <input name="github_username" id="github_username" required value={github_username} onChange={e => setGithubUsername(e.target.value)}/>
         </div>
 
-        <div class="input-block">
+        <div className="input-block">
           <label htmlFor="techs">Tecnologias</label>
-          <input name="techs" id="techs" required/>
+          <input name="techs" id="techs" required value={techs} onChange={e => setTechs(e.target.value)}/>
         </div>
        
        <div className="input-group">
-        <div class="input-block">
+        <div className="input-block">
           <label htmlFor="latitude">Latitude</label>
-          <input name="latitude" id="latitude" required/>
+          <input type="number" name="latitude" id="latitude" required value={latitude} onChange={e => setLatitude(e.target.value)}/>
         </div>
 
-        <div class="input-block">
+        <div className="input-block">
           <label htmlFor="longitude">Longitude</label>
-          <input name="longitude" id="longitude" required/>
+          <input type="number" name="longitude" id="longitude" required value={longitude} onChange={e => setLongitude(e.target.value)}/>
         </div>
        </div>
 
        <button type="submit">Salvar</button>
       </form>
     </aside>
+
     <main>
       <ul>
-        <li className="dev-item">
-          <header>
-            <img src="https://avatars3.githubusercontent.com/u/10124552?s=460&v=4" alt="Sara Lonngren"/>
-            <div className="user-info">
-              <strong>Sara Lonngren</strong>
-              <strong>Ruby on Rails</strong>
-            </div>
-          </header>
-          <p>Backend Developer at Agenda Edu ♥ </p>
-          <a href="https://github.com/SaraLonngren">Acessar perfil no Github</a>
-        </li>
-
-        <li className="dev-item">
-          <header>
-            <img src="https://avatars3.githubusercontent.com/u/10124552?s=460&v=4" alt="Sara Lonngren"/>
-            <div className="user-info">
-              <strong>Sara Lonngren</strong>
-              <strong>Ruby on Rails</strong>
-            </div>
-          </header>
-          <p>Backend Developer at Agenda Edu ♥</p>
-          <a href="https://github.com/SaraLonngren">Acessar perfil no Github</a>
-        </li>
-
-        <li className="dev-item">
-          <header>
-            <img src="https://avatars3.githubusercontent.com/u/10124552?s=460&v=4" alt="Sara Lonngren"/>
-            <div className="user-info">
-              <strong>Sara Lonngren</strong>
-              <strong>Ruby on Rails</strong>
-            </div>
-          </header>
-          <p>Backend Developer at Agenda Edu ♥</p>
-          <a href="https://github.com/SaraLonngren">Acessar perfil no Github</a>
-        </li>
-
-        <li className="dev-item">
-          <header>
-            <img src="https://avatars3.githubusercontent.com/u/10124552?s=460&v=4" alt="Sara Lonngren"/>
-            <div className="user-info">
-              <strong>Sara Lonngren</strong>
-              <strong>Ruby on Rails</strong>
-            </div>
-          </header>
-          <p>Backend Developer at Agenda Edu ♥</p>
-          <a href="https://github.com/SaraLonngren">Acessar perfil no Github</a>
-        </li>
+        {devs.map(dev => (
+           <li key={dev._id} className="dev-item">
+           <header>
+             <img src={dev.avatar_url} alt={dev.name}/>
+             <div className="user-info">
+               <strong>{dev.name}</strong>
+               <span>{dev.techs.join(', ')}</span>
+             </div>
+           </header>
+           <p>{dev.bio}</p>
+           <a href={ `https://github.com/${dev.github_username}`}>Acessar perfil no Github</a>
+         </li>
+        ))}
       </ul>
     </main>
   </div>
